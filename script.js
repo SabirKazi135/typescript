4c713f39aab3f3641b92f819d08d8131634e21a5
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-03 19:18:18 +0530

    commit

--- script.js
+++ script.js
@@ -12,7 +12,29 @@ function addNewPizza(pizzaObj) {
   menu.push(pizzaObj);
 }
 function placeorder(name) {
-
-    
+  const selectedpizza = menu.find((pizzaObj) => pizzaObj.name === name);
+  cashinRegister = selectedpizza.price;
+  const newOrder = {
+    id: nextorderId++,
+    pizza: selectedpizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
 }
 
+function completeOrder(orderId) {
+  const order = orderQueue.find((order) => order.id == orderId);
+  order.status = "completed";
+}
+
+addNewPizza({ name: "pizza5", price: 12 });
+addNewPizza({ name: "pizza6", price: 12 });
+addNewPizza({ name: "pizza7", price: 11 });
+
+placeorder("pizza5");
+completeOrder("1");
+
+console.log("Menu", menu);
+console.log("Cash in register", cashinRegister);
+console.log("Order queue", orderQueue);
