fb31dd5d727b82b2f83beb95d228e371cd9180a8
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-03 19:45:52 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,4 +1,17 @@
-let myName: string = "bob";
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
 
-let numberOfWheels: number = 4;
-let isStudent: boolean = false;
+let person1: Person = {
+  name: "jeo",
+  age: 42,
+  isStudent: true,
+};
+
+let person2: Person = {
+  name: "sabir",
+  age: 22,
+  isStudent: false,
+};
