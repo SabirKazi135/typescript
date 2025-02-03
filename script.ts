86b12084415defe362377b4889001489178e0432
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-03 19:48:39 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,17 +1,45 @@
-type Person = {
+type Pizza = {
   name: string;
-  age: number;
-  isStudent: boolean;
+  price: number;
 };
 
-let person1: Person = {
-  name: "jeo",
-  age: 42,
-  isStudent: true,
-};
+const menu = [
+  { name: "pizza1", price: 8 },
+  { name: "pizza2", price: 10 },
+  { name: "pizza3", price: 10 },
+  { name: "pizza4", price: 9 },
+];
 
-let person2: Person = {
-  name: "sabir",
-  age: 22,
-  isStudent: false,
-};
+const cashinRegister = 100;
+const orderQueue = [];
+
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+}
+function placeorder(name) {
+  const selectedpizza = menu.find((pizzaObj) => pizzaObj.name === name);
+  cashinRegister = selectedpizza.price;
+  const newOrder = {
+    id: nextorderId++,
+    pizza: selectedpizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
+}
+
+function completeOrder(orderId: number) {
+  const order = orderQueue.find((order) => order.id == orderId);
+  order.status = "completed";
+}
+
+addNewPizza({ name: "pizza5", price: 12 });
+addNewPizza({ name: "pizza6", price: 12 });
+addNewPizza({ name: "pizza7", price: 11 });
+
+placeorder("pizza5");
+completeOrder(1);
+
+// console.log("Menu", menu);
+// console.log("Cash in register", cashinRegister);
+// console.log("Order queue", orderQueue);
