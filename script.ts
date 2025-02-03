3db01dd966c183a769f28ac65d9c235a1bdae4f9
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-03 19:38:42 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,42 +1,4 @@
-const menu = [
-  { name: "pizza1", price: 8 },
-  { name: "pizza2", price: 10 },
-  { name: "pizza3", price: 10 },
-  { name: "pizza4", price: 9 },
-];
+let myName: string = "bob";
 
-const cashinRegister = 100;
-const orderQueue = [];
-
-function addNewPizza(pizzaObj) {
-  menu.push(pizzaObj);
-}
-function placeorder(name) {
-  const selectedpizza = menu.find((pizzaObj) => pizzaObj.name === name);
-  if (!selectedpizza) {
-    console.error(`${pizzaname} does not exit`);
-  }
-  cashinRegister = selectedpizza.price;
-  const newOrder = {
-    id: nextorderId++,
-    pizza: selectedpizza,
-    status: "ordered",
-  };
-  orderQueue.push(newOrder);
-  return newOrder;
-}
-function completeOrder(orderId) {
-  const order = orderQueue.find((order) => order.id == orderId);
-  order.status = "completed";
-}
-
-addNewPizza({ name: "pizza5", price: 12 });
-addNewPizza({ name: "pizza6", price: 12 });
-addNewPizza({ name: "pizza7", price: 11 });
-
-placeorder("pizza5");
-completeOrder(1);
-
-console.log("Menu", menu);
-console.log("Cash in register", cashinRegister);
-console.log("Order queue", orderQueue);
+let numberOfWheels: number = 4;
+let isStudent: boolean = false;
