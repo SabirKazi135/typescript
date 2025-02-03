35b7cf5b330edddb47218bd1a82d19944d92fb4c
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-03 21:06:33 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,45 +1,7 @@
-type Pizza = {
-  name: string;
-  price: number;
+const car: { type: string; model: string; year: number } = {
+  type: "Toyota",
+  model: "Corolla",
+  year: 2009,
 };
 
-const menu = [
-  { name: "pizza1", price: 8 },
-  { name: "pizza2", price: 10 },
-  { name: "pizza3", price: 10 },
-  { name: "pizza4", price: 9 },
-];
-
-const cashinRegister = 100;
-const orderQueue = [];
-
-function addNewPizza(pizzaObj) {
-  menu.push(pizzaObj);
-}
-function placeorder(name) {
-  const selectedpizza = menu.find((pizzaObj) => pizzaObj.name === name);
-  cashinRegister = selectedpizza.price;
-  const newOrder = {
-    id: nextorderId++,
-    pizza: selectedpizza,
-    status: "ordered",
-  };
-  orderQueue.push(newOrder);
-  return newOrder;
-}
-
-function completeOrder(orderId: number) {
-  const order = orderQueue.find((order) => order.id == orderId);
-  order.status = "completed";
-}
-
-addNewPizza({ name: "pizza5", price: 12 });
-addNewPizza({ name: "pizza6", price: 12 });
-addNewPizza({ name: "pizza7", price: 11 });
-
-placeorder("pizza5");
-completeOrder(1);
-
-// console.log("Menu", menu);
-// console.log("Cash in register", cashinRegister);
-// console.log("Order queue", orderQueue);
+console.log(car);
