2f0a2de7aad4641f4e04fef7efbfb46cb455a26f
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-03 19:22:54 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,2 +1,39 @@
-let num = 10;
-console.log(`Number is :${num}`);
+const menu = [
+  { name: "pizza1", price: 8 },
+  { name: "pizza2", price: 10 },
+  { name: "pizza3", price: 10 },
+  { name: "pizza4", price: 9 },
+];
+
+const cashinRegister = 100;
+const orderQueue = [];
+
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+}
+function placeorder(name) {
+  const selectedpizza = menu.find((pizzaObj) => pizzaObj.name === name);
+  cashinRegister = selectedpizza.price;
+  const newOrder = {
+    id: nextorderId++,
+    pizza: selectedpizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
+}
+function completeOrder(orderId) {
+  const order = orderQueue.find((order) => order.id == orderId);
+  order.status = "completed";
+}
+
+addNewPizza({ name: "pizza5", price: 12 });
+addNewPizza({ name: "pizza6", price: 12 });
+addNewPizza({ name: "pizza7", price: 11 });
+
+placeorder("pizza5");
+completeOrder("1");
+
+console.log("Menu", menu);
+console.log("Cash in register", cashinRegister);
+console.log("Order queue", orderQueue);
