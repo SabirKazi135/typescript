56ad8c41c348c7ce233f9c50918c1ec328ee08fd
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 09:03:06 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,5 +1,8 @@
-type UserId = number | string;
+type UserId = (number | string)[];
 
-function displayId(id: UserId): string | number {
-  return id;
+function formatUserId(array: UserId): string[] {
+  return array.map((id) => `User ID:${id}`);
 }
+
+console.log(filterNumbers([101, "XYZ123", 202, "ABC456"])); 
+// Output: [101, 202]
