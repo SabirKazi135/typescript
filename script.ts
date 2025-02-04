08d880eec218158bc9cfbdb6474057b0cd24da78
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 09:53:09 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,21 +1,5 @@
-type UserId = (number | string)[];
+let message = "Welcome Back";
+console.log(message);
 
-function filterOddAndLowercase(array: UserId): {
-  numbers: number[];
-  strings: string[];
-} {
-  return {
-    numbers: array.filter((num): num is number => {
-      return typeof num === "number" && num % 2 !== 0;
-    }),
-    strings: array
-      .filter((str): str is string => {
-        return typeof str === "string";
-      })
-      .map((str) => str.toLowerCase()),
-  };
-}
-
-console.log(
-  filterOddAndLowercase([101, "hello", 202, "world", 404, "typescript"])
-);
+let x = 10;
+let x = 130;
