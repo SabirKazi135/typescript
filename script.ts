f5cd0020738e59c8873e05c2b9181b40ed6ec5a3
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 10:08:50 +0530

    commit

--- script.ts
+++ script.ts
@@ -12,6 +12,7 @@ let isBegninner: boolean = true;
 let total: number = 0;
 let Myname: string = "sabir";
 
+Myname = true;
 let sentence: string = `My Myname is ${Myname}`;
 
 console.log(sentence);
