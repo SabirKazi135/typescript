6b4040d2f303a1846178701b744d4f4869098fbd
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 09:31:47 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,8 +1,21 @@
 type UserId = (number | string)[];
 
-function seprate(array: UserId): { numbers: number[], strings: string[] }  {
+function filterEvenAndUppercase(array: UserId): {
+  numbers: number[];
+  strings: string[];
+} {
   return {
-    numbers: Array.filter(num)
-  }
+    numbers: array.filter((num): num is number => {
+      return typeof num === "number" && num % 2 === 0;
+    }),
+    strings: array
+      .filter((str): str is string => {
+        return typeof str === "string";
+      })
+      .map((str) => str.toUpperCase()),
+  };
 }
 
+console.log(
+  filterEvenAndUppercase([101, "hello", 202, "world", 404, "typescript"])
+);
