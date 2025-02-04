930dd77364746cec44c2a3ccc98dcdd778684b74
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 15:55:53 +0530

    commit

--- script.ts
+++ script.ts
@@ -12,3 +12,18 @@ class Employee {
 let emp1 = new Employee("vishwas");
 console.log(emp1.employeeName);
 emp1.greet();
+
+class Manager extends Employee {
+  constructor(managerName: string) {
+    super(managerName);
+  }
+
+  delegateWork() {
+    console.log(`Manager delegating tasks`);
+  }
+}
+
+let m1 = new Manager("sabir");
+m1.delegateWork();
+console.log(m1.employeeName);
+m1.greet();
