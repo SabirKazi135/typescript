211423db160686b1c60cf3d6d280faffeeffd858
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 08:50:19 +0530

    commit

--- script.ts
+++ script.ts
@@ -1,6 +1,5 @@
 type UserId = number | string;
 
-let sabir: UserId = 1;
-let raees: UserId = "ab123";
-
-console.log(sabir, raees);
+function displayId(id: UserId): string | number {
+  return id;
+}
