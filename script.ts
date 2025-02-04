ead7f17d0caadc9917a1eb6c76d2befdefa7e88b
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 10:14:06 +0530

    commit

--- script.ts
+++ script.ts
@@ -4,5 +4,5 @@ let u: undefined = undefined;
 let list1: number[] = [1, 2, 3, 4, 5];
 console.log(list1);
 
-let person1: [string, number] = ["sabir", 4];
+let person1: [string, number] = ["sabir", 4, 6];
 console.log(person1);
