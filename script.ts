2d839841d83d0a733d1f3a2a92dcf1b10a509905
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-04 20:12:38 +0530

    commit

--- script.ts
+++
@@ -1,7 +0,0 @@
-function returnValue<T>(arr: T[]): T {
-  return arr[0];
-}
-
-console.log(returnValue<string>(["sabir", "raees"]));
-console.log(returnValue<number>([10, 12]));
-console.log(returnValue<boolean>([false, true]));
