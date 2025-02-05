76bc3dc973187810dc61c713612352b15b78f510
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-05 13:59:00 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const heading = document.querySelector("h1");
 if (heading) {
-  heading.textContent = "Hello from TypeScript!";
+  heading.textContent = "Hello sabir from TypeScript!";
 }
 
 console.log("TypeScript is working!");
